deef1099c04a31447a312e061a87f966cdc190d5
SymplyMatt <symplymatt@gmail.com>
2024-02-26 23:02:50 +0100

    reusable components

--- src/components/welcome/Nav.vue
+++ src/components/welcome/Nav.vue
@@ -1,10 +1,10 @@
 <template>
     <Container :classes="`w-full flex justify-between items-center gap-20 absolute top-[20px] px-[20px]`">
         <Logo />
-        <div class="flex justify-center items-center ex-sm-phone:text-[12px] text-[7px] sm-phone:text-[16px] phone-big:text-[18px] gap-5 phone-big:gap-10 font-bold">
+        <Container :classes="`flex justify-center items-center ex-sm-phone:text-[12px] text-[7px] sm-phone:text-[16px] phone-big:text-[18px] gap-5 phone-big:gap-10 font-bold`">
             <Button :text="`LOG IN`" :classes="`p-10 ex-sm-phone:p-10 bg-inherit rounded-10`" :onClickFunction="handleClick"/>
             <Button :text='`SIGN UP`' :classes="`p-[8px] ex-sm-phone:p-10 bg-[#4CE895] rounded-10`" :onClickFunction="handleClick"/>
-        </div>
+        </Container>
     </Container>
 </template>
 
