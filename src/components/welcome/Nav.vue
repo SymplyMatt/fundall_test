e37580cace0d22abbdc63aa4b97029a48c02d86e
SymplyMatt <symplymatt@gmail.com>
2024-02-27 18:05:34 +0100

    go to login and go to signup

--- src/components/welcome/Nav.vue
+++ src/components/welcome/Nav.vue
@@ -2,18 +2,25 @@
     <Container :classes="`w-full flex justify-between items-center gap-20 absolute top-[20px] px-[20px]`">
         <Logo />
         <Container :classes="`flex justify-center items-center ex-sm-phone:text-[12px] text-[7px] sm-phone:text-[16px] phone-big:text-[18px] gap-5 phone-big:gap-10 font-bold`">
-            <Button :text="`LOG IN`" :classes="`p-10 ex-sm-phone:p-10 bg-inherit rounded-10`" :onClickFunction="handleClick"/>
-            <Button :text='`SIGN UP`' :classes="`p-[8px] ex-sm-phone:p-10 bg-[#4CE895] rounded-10`" :onClickFunction="handleClick"/>
+            <Button :text="`LOG IN`" :classes="`p-10 ex-sm-phone:p-10 bg-inherit rounded-10`" :onClickFunction="goToLogin"/>
+            <Button :text='`SIGN UP`' :classes="`p-[8px] ex-sm-phone:p-10 bg-[#4CE895] rounded-10`" :onClickFunction="goToSignup"/>
         </Container>
     </Container>
 </template>
 
 <script setup>
+    import { useRouter } from 'vue-router'
     import Logo from '../common/Logo.vue'
     import Button from '../common/Button.vue'
     import Container from '../common/Container.vue'
 
-    const handleClick = () => {
-        console.log('I was logged');
-    };
+    const router = useRouter()
+
+    const goToLogin = () => {
+        router.push('/login')
+    }
+
+    const goToSignup = () => {
+        router.push('/signup')
+    }
 </script>
