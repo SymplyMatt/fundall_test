a28f02a7d8a46c40d78b0d937bdd705e93fc5443
SymplyMatt <symplymatt@gmail.com>
2024-02-28 13:37:18 +0100

    register endpoint

--- src/components/auth/SignUpForm.vue
+++ src/components/auth/SignUpForm.vue
@@ -6,27 +6,27 @@
                     <Container :classes="`w-full flex flex-col sm-phone:flex-row gap-20`">
                         <Container :classes="`w-full flex flex-col gap-5`">
                             <Text :text="`First Name`" :classes="`font-semibold text-[12px] lg:text-[14px]`"/>
-                            <Input :placeholder="`First Name`"/>
+                            <Input :placeholder="`First Name`" :modelValue="`firstname`" @updateModel="update_model"/>
                         </Container>
                         <Container :classes="`w-full flex flex-col gap-5`">
                             <Text :text="`Last Name`" :classes="`font-semibold text-[12px] lg:text-[14px]`"/>
-                            <Input :placeholder="`Last Name`"/>
+                            <Input :placeholder="`Last Name`" :modelValue="`lastname`" @updateModel="update_model"/>
                         </Container>
                     </Container>
                     <Container :classes="`w-full flex flex-col gap-5`">
                         <Text :text="`Email address`" :classes="`font-semibold text-[12px] lg:text-[14px]`"/>
-                        <Input :placeholder="`Enter Email`"/>
+                        <Input :placeholder="`Enter Email`" :modelValue="`email`" @updateModel="update_model"/>
                     </Container>
                     <Container :classes="`w-full flex flex-col gap-5`">
                         <Text :text="`Password`" :classes="`font-semibold text-[12px] lg:text-[14px]`"/>
-                        <Input :placeholder="`Enter Password`"/>
+                        <Input :placeholder="`Enter Password`" :modelValue="`password`" @updateModel="update_model"/>
                     </Container>
                     <Container :classes="`w-full flex flex-col gap-5`">
                         <Text :text="`Confirm Password`" :classes="`font-semibold text-[12px] lg:text-[14px]`"/>
-                        <Input :placeholder="`Confirm Password`"/>
+                        <Input :placeholder="`Confirm Password`" :modelValue="`password_confirmation`" @updateModel="update_model"/>
                     </Container>
                 </Container>
-                <Button :text='`SIGN UP`' :classes="`flex justify-center items-center p-[8px] ex-sm-phone:p-10 bg-[#4CE895] h-[45px] font-bold`" :onClickFunction="goToDashboard" />
+                <Button :text='`SIGN UP`' :classes="`flex justify-center items-center p-[8px] ex-sm-phone:p-10 bg-[#4CE895] h-[45px] font-bold`" :onClickFunction="authUser" :active="areInputsValid" :loading="loading" />
             </Container>
             <Container :classes="`font-semibold text-[12px] lg:text-[14px] text-center`">
                 <span :class="`text-black font-normal`">Already have an account? &nbsp;</span>
@@ -43,6 +43,9 @@
     import Button from '../common/Button.vue'
     import Input from '../common/Input.vue';
     import { useRouter } from 'vue-router'
+    import sendRequest from '../../config/fetchData.js'
+    import { ref, watch, computed } from 'vue';
+
     const router = useRouter();
 
     const goToLogin = () => {
@@ -51,4 +54,50 @@
     const goToDashboard = () => {
         router.push('/dashboard');
     }
+    const values = ref({
+        firstname : '',
+        lastname : '',
+        email : '',
+        password : '',
+        password_confirmation : '',
+    });
+    const loading = ref(false);
+
+    const update_model = (newValue) =>{
+        const {key, value} = newValue
+        values.value = {...values.value, [key]: value}
+    }
+
+    const areInputsValid = computed(() => {
+        const isValid = Boolean(values.value.firstname && values.value.lastname && isValidEmail(values.value.email || '') && values.value.password.length > 5 && values.value.password_confirmation.length > 5) ;
+        return isValid;
+    });
+    function isValidEmail(email) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+    
+    const authUser = async () =>{
+        if(areInputsValid.value){
+            try {
+            loading.value = true;
+            const response = await sendRequest('post', '/api/v1/register', values.value);
+            if (response.status === 200) {
+              loading.value = false;
+              goToDashboard();
+            } else {
+              loading.value = false; 
+            }
+          } catch (error) {
+            console.error('An error occurred while fetching data', error);
+            loading.value = false;
+          }
+        }else{
+            console.log('invalid form inputs');
+        }
+    }
+
+    watch(() => values.value, (newValue) => {
+        // console.log('values: ', newValue);
+    });
 </script>
